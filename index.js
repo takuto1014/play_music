f5c22ccc87c61692dbc524c86a554b460a9e7f9e
takuto1014 <76800704+takuto1014@users.noreply.github.com>
2021-11-18 10:20:37 +0900

    Add files via upload

---
+++ index.js
@@ -0,0 +1,352 @@
+var music00 = new Audio("00_消えた絵画.mp3");
+music00.volume = 2.0
+var music10 = new Audio("10_死後の世界.mp3");
+music10.volume = 2.0
+var music20 = new Audio("20_死後の世界.mp3");
+music20.volume = 2.0
+var music21 = new Audio("21_歓声.mp3");
+music21.volume = 2.0
+var music22 = new Audio("22_火が燃える.mp3");
+music22.volume = 2.0
+var music23 = new Audio("23_鐘.mp3");
+music23.volume = 2.0
+var music24 = new Audio("24_消えた絵画.mp3");
+music24.volume = 2.0
+var music31 = new Audio("31_ドアノック-開ける.mp3");
+music31.volume = 2.0
+var music41 = new Audio("41_暗がりから伸びる手.mp3");
+music41.volume = 2.0
+var music43 = new Audio("43_電話バイブ-取る.mp3");
+music43.volume = 2.0
+var music51 = new Audio("51_缶を開ける.mp3");
+music51.volume = 2.0
+var music70 = new Audio("70_死後の世界.mp3");
+music70.volume = 2.0
+var music71 = new Audio("71_軍隊の行進.mp3");
+music71.volume = 2.0
+var music72 = new Audio("72_処刑用BGM.mp3");
+music72.volume = 2.0
+var music73 = new Audio("73_銃を構える_.mp3");
+music73.volume = 2.0
+var music74 = new Audio("74_小歓声.mp3");
+music74.volume = 2.0
+var music75 = new Audio("75_タイムワープto過去.mp3");
+music75.volume = 2.0
+var music77 = new Audio("77_大歓声.mp3");
+music77.volume = 2.0
+var music78 = new Audio("78_火が燃える.mp3");
+music78.volume = 2.0
+var music80 = new Audio("80_泡沫の夢.mp3");
+music80.volume = 2.0
+var music81 = new Audio("81_タイムワープto現在.mp3");
+music81.volume = 2.0
+var music91 = new Audio("91_銃声.mp3");
+music91.volume = 2.0
+var music92 = new Audio("92_END.mp3");
+music92.volume = 2.0
+var music99 = new Audio("99_ざわざわ.mp3");
+music99.volume = 2.0
+document.getElementById("audio00").addEventListener("click", function() {
+    if(music00.currentTime==0){
+        music00.play()
+    }else{
+        music00.pause()
+        music00.currenTime=0
+    }
+}, false);
+document.getElementById("audio10").addEventListener("click", function() {
+    if(music10.currentTime==0){
+        music10.play()
+    }else{
+        music10.pause()
+        music10.currenTime=0
+    }
+}, false);
+document.getElementById("audio20").addEventListener("click", function() {
+    if(music20.currentTime==0){
+        music20.play()
+    }else{
+        music20.pause()
+        music20.currenTime=0
+    }
+}, false);
+document.getElementById("audio21").addEventListener("click", function() {
+    if(music21.currentTime==0){
+        music21.play()
+    }else{
+        music21.pause()
+        music21.currenTime=0
+    }
+}, false);
+document.getElementById("audio22").addEventListener("click", function() {
+    if(music22.currentTime==0){
+        music22.play()
+    }else{
+        music22.pause()
+        music22.currenTime=0
+    }
+}, false);
+document.getElementById("audio23").addEventListener("click", function() {
+    if(music23.currentTime==0){
+        music23.play()
+    }else{
+        music23.pause()
+        music23.currenTime=0
+    }
+}, false);
+document.getElementById("audio24").addEventListener("click", function() {
+    if(music24.currentTime==0){
+        music24.play()
+    }else{
+        music24.pause()
+        music24.currenTime=0
+    }
+}, false);
+document.getElementById("audio31").addEventListener("click", function() {
+    if(music31.currentTime==0){
+        music31.play()
+    }else{
+        music31.pause()
+        music31.currenTime=0
+    }
+}, false);
+document.getElementById("audio41").addEventListener("click", function() {
+    if(music41.currentTime==0){
+        music41.play()
+    }else{
+        music41.pause()
+        music41.currenTime=0
+    }
+}, false);
+document.getElementById("audio43").addEventListener("click", function() {
+    if(music43.currentTime==0){
+        music43.play()
+    }else{
+        music43.pause()
+        music43.currenTime=0
+    }
+}, false);
+document.getElementById("audio51").addEventListener("click", function() {
+    if(music51.currentTime==0){
+        music51.play()
+    }else{
+        music51.pause()
+        music51.currenTime=0
+    }
+}, false);
+document.getElementById("audio70").addEventListener("click", function() {
+    if(music70.currentTime==0){
+        music70.play()
+    }else{
+        music70.pause()
+        music70.currenTime=0
+    }
+}, false);
+document.getElementById("audio71").addEventListener("click", function() {
+    if(music71.currentTime==0){
+        music71.play()
+    }else{
+        music71.pause()
+        music71.currenTime=0
+    }
+}, false);
+document.getElementById("audio72").addEventListener("click", function() {
+    if(music72.currentTime==0){
+        music72.play()
+    }else{
+        music72.pause()
+        music72.currenTime=0
+    }
+}, false);
+document.getElementById("audio73").addEventListener("click", function() {
+    if(music73.currentTime==0){
+        music73.play()
+    }else{
+        music73.pause()
+        music73.currenTime=0
+    }
+}, false);
+document.getElementById("audio74").addEventListener("click", function() {
+    if(music74.currentTime==0){
+        music74.play()
+    }else{
+        music74.pause()
+        music74.currenTime=0
+    }
+}, false);
+document.getElementById("audio75").addEventListener("click", function() {
+    if(music75.currentTime==0){
+        music75.play()
+    }else{
+        music75.pause()
+        music75.currenTime=0
+    }
+}, false);
+document.getElementById("audio77").addEventListener("click", function() {
+    if(music77.currentTime==0){
+        music77.play()
+    }else{
+        music77.pause()
+        music77.currenTime=0
+    }
+}, false);
+document.getElementById("audio78").addEventListener("click", function() {
+    if(music78.currentTime==0){
+        music78.play()
+    }else{
+        music78.pause()
+        music78.currenTime=0
+    }
+}, false);
+document.getElementById("audio80").addEventListener("click", function() {
+    if(music80.currentTime==0){
+        music80.play()
+    }else{
+        music80.pause()
+        music80.currenTime=0
+    }
+}, false);
+document.getElementById("audio81").addEventListener("click", function() {
+    if(music81.currentTime==0){
+        music81.play()
+    }else{
+        music81.pause()
+        music81.currenTime=0
+    }
+}, false);
+document.getElementById("audio91").addEventListener("click", function() {
+    if(music91.currentTime==0){
+        music91.play()
+    }else{
+        music91.pause()
+        music91.currenTime=0
+    }
+}, false);
+document.getElementById("audio92").addEventListener("click", function() {
+    if(music92.currentTime==0){
+        music92.play()
+    }else{
+        music92.pause()
+        music92.currenTime=0
+    }
+}, false);
+document.getElementById("audio99").addEventListener("click", function() {
+    if(music99.currentTime==0){
+        music99.play()
+    }else{
+        music99.pause()
+        music99.currenTime=0
+    }
+}, false);
+// document.getElementById("audio0").addEventListener("click", function () {
+//     if(music0.currentTime==0){
+//         music0.play()
+//     }else{
+//         music0.pause()
+//         music0.currentTime=0
+//     }
+// }, false);
+
+document.getElementById("audio_pause").addEventListener("click", function () {
+if(music00.currenTime!=0){
+    music00.pause()
+    music00.currentTime=0
+}
+if(music10.currenTime!=0){
+    music10.pause()
+    music10.currentTime=0
+}
+if(music20.currenTime!=0){
+    music20.pause()
+    music20.currentTime=0
+}
+if(music21.currenTime!=0){
+    music21.pause()
+    music21.currentTime=0
+}
+if(music22.currenTime!=0){
+    music22.pause()
+    music22.currentTime=0
+}
+if(music23.currenTime!=0){
+    music23.pause()
+    music23.currentTime=0
+}
+if(music24.currenTime!=0){
+    music24.pause()
+    music24.currentTime=0
+}
+if(music31.currenTime!=0){
+    music31.pause()
+    music31.currentTime=0
+}
+if(music41.currenTime!=0){
+    music41.pause()
+    music41.currentTime=0
+}
+if(music43.currenTime!=0){
+    music43.pause()
+    music43.currentTime=0
+}
+if(music51.currenTime!=0){
+    music51.pause()
+    music51.currentTime=0
+}
+if(music70.currenTime!=0){
+    music70.pause()
+    music70.currentTime=0
+}
+if(music71.currenTime!=0){
+    music71.pause()
+    music71.currentTime=0
+}
+if(music72.currenTime!=0){
+    music72.pause()
+    music72.currentTime=0
+}
+if(music73.currenTime!=0){
+    music73.pause()
+    music73.currentTime=0
+}
+if(music74.currenTime!=0){
+    music74.pause()
+    music74.currentTime=0
+}
+if(music75.currenTime!=0){
+    music75.pause()
+    music75.currentTime=0
+}
+if(music77.currenTime!=0){
+    music77.pause()
+    music77.currentTime=0
+}
+if(music78.currenTime!=0){
+    music78.pause()
+    music78.currentTime=0
+}
+if(music80.currenTime!=0){
+    music80.pause()
+    music80.currentTime=0
+}
+if(music81.currenTime!=0){
+    music81.pause()
+    music81.currentTime=0
+}
+if(music91.currenTime!=0){
+    music91.pause()
+    music91.currentTime=0
+}
+if(music92.currenTime!=0){
+    music92.pause()
+    music92.currentTime=0
+}
+if(music99.currenTime!=0){
+    music99.pause()
+    music99.currentTime=0
+}
+    // if(music0.currentTime!=0){
+    //     music0.pause()
+    //     music0.currenTime=0
+    // }
+    
+}, false);
